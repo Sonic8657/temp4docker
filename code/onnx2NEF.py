110f1906ab02a379309a8ada57df7c8462686e39
Sprite777 <benson199757@gmail.com>
2022-11-01 15:52:14 +0800

    commit

---
+++ code/onnx2NEF.py
@@ -0,0 +1,33 @@
+import ktc
+import numpy as np
+import os
+import onnx
+from PIL import Image
+onnx_path = '/data1/temp4docker/yolov3-tiny-OP.onnx'
+m = onnx.load(onnx_path)
+# npu (only) performance simulation
+km = ktc.ModelConfig(20008, "0001", "520", onnx_model=m)
+eval_result = km.evaluate()
+print("\nNpu performance evaluation result:\n" + str(eval_result))
+
+from os import walk
+
+img_list = []
+for (dirpath, dirnames, filenames) in walk("/data1/temp4docker/voc_data50"):
+    for f in filenames:
+        fullpath = os.path.join(dirpath, f)
+        
+        image = Image.open(fullpath)
+        image = image.convert("RGB")
+        image = Image.fromarray(np.array(image)[...,::-1])
+        img_data = np.array(image.resize((640, 640), Image.BILINEAR)) / 256 - 0.5
+        print(fullpath)
+        img_list.append(img_data)
+        
+# fixed-point analysis
+bie_model_path = km.analysis({"input": img_list})
+print("\nFixed-point analysis done. Saved bie model to '" + str(bie_model_path) + "'")
+
+# compile
+nef_model_path = ktc.compile([km])
+print("\nCompile done. Saved Nef file to '" + str(nef_model_path) + "'")
