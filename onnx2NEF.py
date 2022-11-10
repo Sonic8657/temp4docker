61e72ebd3f5e1f1fc668ed426294e35cce56007e
Sprite777 <benson199757@gmail.com>
2022-11-10 17:02:33 +0800

    commit

--- onnx2NEF.py
+++ onnx2NEF.py
@@ -1,19 +1,20 @@
 import torch
 import torch.onnx
 import ktc
-
-km = ktc.ModelConfig(32769, "0001", "520", onnx_path="/workspace/temp4docker/resnet_50_Opt.onnx")
-eval_result = km.evaluate()
-
 from PIL import Image
 import numpy as np
+
 
 def preprocess(input_file):
     image = Image.open(input_file)
     image = image.convert("RGB")
     img_data = np.array(image.resize((256, 256), Image.BILINEAR)) / 255
-    img_data = np.transpose(img_data, (1, 0, 2))
+    #img_data = np.transpose(img_data, (1, 0, 2))
     return img_data
+
+
+km = ktc.ModelConfig(32769, "0001", "520", onnx_path="/workspace/temp4docker/resnet_50_Opt.onnx")
+#eval_result = km.evaluate()
 
 input_data = [preprocess("/workspace/temp4docker/image/0_webcam_1.jpg")]
 inf_results = ktc.kneron_inference(input_data, onnx_file="/workspace/data1/resnet_50_Opt.onnx", input_names=["data_out"])
