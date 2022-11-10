5a4ed94dfa5b07979c3852803291bcb16c7eb6ea
Sprite777 <benson199757@gmail.com>
2022-11-10 16:35:40 +0800

    commit

---
+++ onnx2NEF.py
@@ -0,0 +1,32 @@
+
+km = ktc.ModelConfig(32769, "0001", "520", onnx_path="/workspace/temp4docker/resnet_50_Opt.onnx")
+eval_result = km.evaluate()
+
+from PIL import Image
+import numpy as np
+
+def preprocess(input_file):
+    image = Image.open(input_file)
+    image = image.convert("RGB")
+    img_data = np.array(image.resize((256, 256), Image.BILINEAR)) / 255
+    img_data = np.transpose(img_data, (1, 0, 2))
+    return img_data
+
+input_data = [preprocess("/workspace/temp4docker/image/0_webcam_1.jpg")]
+inf_results = ktc.kneron_inference(input_data, onnx_file="/workspace/data1/resnet_50_Opt.onnx", input_names=["data_out"])
+
+
+analysis(input_mapping, output_bie = None, threads = 4, mode=1)
+# Preprocess images and create the input mapping
+input_images = [
+    preprocess("/workspace/temp4docker/image/0_webcam_1.jpg"),
+    preprocess("/workspace/temp4docker/image/0_webcam_2.jpg"),
+    preprocess("/workspace/temp4docker/image/0_webcam_3.jpg"),
+    preprocess("/workspace/temp4docker/image/0_webcam_4.jpg"),
+]
+input_mapping = {"data_out": input_images}
+
+# Quantization
+bie_path = km.analysis(input_mapping, output_bie = None, threads = 4)
+
+compile_result = ktc.compile([km])
